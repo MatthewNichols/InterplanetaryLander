77046a160c42afe0ba9093a9110ca8e970ab7bde
Matthew Nichols <matthew@matthew-nichols.com>
2015-08-28 17:33:57 -0600

    Created a project to test different HTML5 game engines.

---
+++ Experiments/GameEngineExperiments/GameEngineExperiments/Scripts/PageScripts/Phaser/FallingBall.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="../../typings/phaser/phaser.d.ts" />
---
+++ Experiments/GameEngineExperiments/GameEngineExperiments/Scripts/PageScripts/Phaser/FallingBall.ts
@@ -0,0 +1,33 @@
+﻿/// <reference path="../../typings/phaser/phaser.d.ts" />
+
+require.config({
+    baseUrl: '../..',
+
+    paths: {
+        'jquery': '/Scripts/jquery-2.1.4.min',
+        'phaser': '/Scripts/phaser/phaser.min'
+    }
+});
+
+require(["phaser", "jquery"], function (phaser, $) {
+
+    console.log("Hi");
+
+    let game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: preload, create: create, render: render });
+
+    function preload() {
+        console.log('preload');
+        game.load.image('ship', '/Content/images/ship.png');
+        
+    }
+
+    function create() {
+        console.log("create");
+        game.add.sprite(game.world.centerX, game.world.centerY, "ship");
+    }
+
+    function render() {
+        //console.log("render");
+    }
+
+});
