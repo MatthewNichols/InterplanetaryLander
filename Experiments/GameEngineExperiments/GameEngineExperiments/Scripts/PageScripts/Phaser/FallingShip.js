b9debd3bbcd86fd4b3549c644b85184b0dfe29d9
Matthew Nichols <matthew@matthew-nichols.com>
2015-10-03 22:02:32 -0600

    Refactoring running state to a distinct class.

--- Experiments/GameEngineExperiments/GameEngineExperiments/Scripts/PageScripts/Phaser/FallingShip.js
+++ Experiments/GameEngineExperiments/GameEngineExperiments/Scripts/PageScripts/Phaser/FallingShip.js
@@ -1,4 +1,9 @@
 /// <reference path="../../typings/phaser/phaser.d.ts" />
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 require.config({
     baseUrl: '../..',
     paths: {
@@ -7,100 +12,110 @@ require.config({
     }
 });
 require(["phaser", "jquery"], function (phaser, $) {
+    var maxSafeVelocity = 20;
+    var maxThrust = 300;
     var colors = {
         Red: '#ff0044', Green: '#069214', Yellow: '#e7f24a'
     };
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: preload, create: create, render: render });
-    var maxSafeVelocity = 20;
-    var maxThrust = 300;
-    var shipSprite;
-    var groundColider;
-    var velocityDisplay;
-    var onGround = false;
-    var thrusting = false;
-    var cursors;
-    var explosionSound;
-    function preload() {
-        console.log('preload');
-        game.load.spritesheet('ship', '/Content/images/shipSpriteSheet.png', 30, 40, 11, 0, 1);
-        game.load.image("ground", '/Content/images/ground.png');
-        game.load.image("groundBlank", '/Content/images/groundBlank.png');
-        game.load.audio('explosion', '/Content/sounds/explosion.mp3');
-        //Attribution: http://soundbible.com/1986-Bomb-Exploding.html
-    }
-    function create() {
-        console.log("create");
-        var worldWidth = game.world.width;
-        var worldHeight = game.world.height;
-        explosionSound = game.add.audio('explosion');
-        game.physics.startSystem(Phaser.Physics.P2JS);
-        game.physics.p2.gravity.y = 100;
-        shipSprite = game.add.sprite(game.world.centerX, 30, "ship");
-        shipSprite.animations.add("fireRocket", [1, 2, 3, 2], 3, true);
-        shipSprite.animations.add("explodeShip", [4, 5, 6, 7, 8, 9, 10], 3, false);
-        game.physics.enable(shipSprite, Phaser.Physics.P2JS);
-        game.add.tileSprite(0, worldHeight - 18, worldWidth, 18, 'ground');
-        groundColider = game.add.tileSprite(worldWidth / 2, worldHeight, worldWidth, 11, 'groundBlank');
-        game.physics.enable(groundColider, Phaser.Physics.P2JS);
-        groundColider.body.static = true;
-        shipSprite.body.onBeginContact.add(function (body, bodyB, shapeA, shapeB, equation) {
-            console.log('contact', shipSprite.body.velocity.y);
-            landed();
-        }, this);
-        velocityDisplay = game.add.text(10, 10, "Velocity: 0", {
-            font: '14px Arial',
-            fill: '#ff0044',
-            align: 'left'
-        });
-        cursors = game.input.keyboard.createCursorKeys();
-    }
-    function render() {
-        //console.log("render");
-        if (cursors.up.isDown) {
-            thrust();
+    var GameRunningState = (function (_super) {
+        __extends(GameRunningState, _super);
+        function GameRunningState() {
+            _super.call(this);
+            this.onGround = false;
+            this.thrusting = false;
         }
-        else if (thrusting) {
-            stopThrust();
+        GameRunningState.prototype.preload = function () {
+            this.game.load.spritesheet('ship', '/Content/images/shipSpriteSheet.png', 30, 40, 11, 0, 1);
+            this.game.load.image("ground", '/Content/images/ground.png');
+            this.game.load.image("groundBlank", '/Content/images/groundBlank.png');
+            this.game.load.audio('explosion', '/Content/sounds/explosion.mp3');
+            //Attribution: http://soundbible.com/1986-Bomb-Exploding.html
+        };
+        GameRunningState.prototype.create = function () {
+            var _this = this;
+            var worldWidth = this.game.world.width;
+            var worldHeight = this.game.world.height;
+            this.explosionSound = this.game.add.audio('explosion');
+            this.game.physics.startSystem(Phaser.Physics.P2JS);
+            this.game.physics.p2.gravity.y = 100;
+            this.shipSprite = this.game.add.sprite(this.game.world.centerX, 30, "ship");
+            this.shipSprite.animations.add("fireRocket", [1, 2, 3, 2], 3, true);
+            this.shipSprite.animations.add("explodeShip", [4, 5, 6, 7, 8, 9, 10], 3, false);
+            this.game.physics.enable(this.shipSprite, Phaser.Physics.P2JS);
+            this.game.add.tileSprite(0, worldHeight - 18, worldWidth, 18, 'ground');
+            this.groundColider = this.game.add.tileSprite(worldWidth / 2, worldHeight, worldWidth, 11, 'groundBlank');
+            this.game.physics.enable(this.groundColider, Phaser.Physics.P2JS);
+            this.groundColider.body.static = true;
+            this.shipSprite.body.onBeginContact.add(function (body, bodyB, shapeA, shapeB, equation) {
+                //console.log('contact', shipSprite.body.velocity.y);
+                _this.landed();
+            }, this);
+            this.velocityDisplay = this.game.add.text(10, 10, "Velocity: 0", {
+                font: '14px Arial',
+                fill: '#ff0044',
+                align: 'left'
+            });
+            this.cursors = this.game.input.keyboard.createCursorKeys();
+        };
+        //update() {
+        //}
+        GameRunningState.prototype.render = function () {
+            if (this.cursors.up.isDown) {
+                this.thrust();
+            }
+            else if (this.thrusting) {
+                this.stopThrust();
+            }
+            if (!this.onGround) {
+                this.displayFlightData();
+            }
+        };
+        GameRunningState.prototype.displayFlightData = function () {
+            var speed = this.shipSprite.body.velocity.y;
+            if (speed > maxSafeVelocity) {
+                console.log("greater than " + maxSafeVelocity);
+                this.velocityDisplay.fill = colors.Red;
+            }
+            else if (speed < maxSafeVelocity && speed >= 0) {
+                console.log("less than " + maxSafeVelocity);
+                this.velocityDisplay.fill = colors.Green;
+            }
+            else if (speed < 0) {
+                this.velocityDisplay.fill = colors.Yellow;
+            }
+            this.velocityDisplay.setText("Velocity: " + speed);
+        };
+        GameRunningState.prototype.thrust = function () {
+            this.shipSprite.body.thrust(maxThrust);
+            this.shipSprite.animations.play("fireRocket");
+            this.thrusting = true;
+        };
+        GameRunningState.prototype.stopThrust = function () {
+            this.shipSprite.animations.stop("fireRocket");
+            this.shipSprite.animations.frame = 0;
+            this.thrusting = false;
+        };
+        GameRunningState.prototype.landed = function () {
+            this.onGround = true;
+            if (this.shipSprite.body.velocity.y > 10) {
+                this.shipSprite.animations.play("explodeShip");
+                this.explosionSound.play();
+                var crashedText = this.game.add.text(this.game.world.centerX, this.game.world.centerY, "You Crashed!", { font: '50px Arial', fill: '#ff0044', align: 'center' });
+                crashedText.anchor.set(0.5);
+            }
+        };
+        return GameRunningState;
+    })(Phaser.State);
+    var FallingShip = (function () {
+        function FallingShip() {
+            this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content');
+            this.game.state.add("RunningState", GameRunningState, false);
+            this.game.state.start("RunningState", true, true);
         }
-        if (!onGround) {
-            displayFlightData();
-        }
-    }
-    function displayFlightData() {
-        var speed = shipSprite.body.velocity.y;
-        if (speed > maxSafeVelocity) {
-            console.log("greater than " + maxSafeVelocity);
-            velocityDisplay.fill = colors.Red;
-        }
-        else if (speed < maxSafeVelocity && speed >= 0) {
-            console.log("less than " + maxSafeVelocity);
-            velocityDisplay.fill = colors.Green;
-        }
-        else if (speed < 0) {
-            velocityDisplay.fill = colors.Yellow;
-        }
-        velocityDisplay.setText("Velocity: " + speed);
-    }
-    function thrust() {
-        shipSprite.body.thrust(maxThrust);
-        shipSprite.animations.play("fireRocket");
-        thrusting = true;
-    }
-    function stopThrust() {
-        shipSprite.animations.stop("fireRocket");
-        shipSprite.animations.frame = 0;
-        thrusting = false;
-    }
-    function landed() {
-        onGround = true;
-        if (shipSprite.body.velocity.y > 10) {
-            shipSprite.animations.play("explodeShip");
-            explosionSound.play();
-            var crashedText = game.add.text(game.world.centerX, game.world.centerY, "You Crashed!", { font: '50px Arial', fill: '#ff0044', align: 'center' });
-            crashedText.anchor.set(0.5);
-        }
-    }
-    $('#pause').click(function () { return game.physics.p2["isPaused"] = true; });
-    $('#resume').click(function () { return game.physics.p2["isPaused"] = false; });
+        return FallingShip;
+    })();
+    var runningGame = new FallingShip();
+    //$('#pause').click(() => game.physics.p2["isPaused"] = true);
+    //$('#resume').click(() => game.physics.p2["isPaused"] = false);
 });
 //# sourceMappingURL=FallingShip.js.map
