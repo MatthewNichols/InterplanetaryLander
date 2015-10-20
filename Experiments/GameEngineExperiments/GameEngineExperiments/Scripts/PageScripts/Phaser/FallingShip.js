2fcb9405642aaebb1770b1c67a9c99b75bf2c057
Matthew Nichols <matthew@matthew-nichols.com>
2015-10-19 19:46:43 -0600

    Added CodeMirror for the editor.  Cleaned up layout a bit.

--- Experiments/GameEngineExperiments/GameEngineExperiments/Scripts/PageScripts/Phaser/FallingShip.js
+++ Experiments/GameEngineExperiments/GameEngineExperiments/Scripts/PageScripts/Phaser/FallingShip.js
@@ -1,13 +1,21 @@
 /// <reference path="../../typings/phaser/phaser.d.ts" />
+/// <reference path="../../typings/codemirror/codemirror.d.ts" />
 define(["require", "exports", "FallingShipIntroState", "FallingShipRunningState"], function (require, exports, introState, runningState) {
     var FallingShip;
     (function (FallingShip_1) {
         var FallingShip = (function () {
             function FallingShip() {
-                this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content');
+                //this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content');
+                this.game = new Phaser.Game("75%", 600, Phaser.AUTO, 'content');
                 this.game.state.add("IntroState", introState.GameIntroState, false);
                 this.game.state.add("RunningState", runningState.GameRunningState, false);
                 this.game.state.start("IntroState", true, true);
+                CodeMirror(document.getElementById('codeArea'), {
+                    value: "console.log('hello world');",
+                    mode: "javascript",
+                    theme: "base16-dark",
+                    lineNumbers: true
+                });
             }
             return FallingShip;
         })();
--- Experiments/GameEngineExperiments/GameEngineExperiments/Scripts/PageScripts/Phaser/FallingShip.ts
+++ Experiments/GameEngineExperiments/GameEngineExperiments/Scripts/PageScripts/Phaser/FallingShip.ts
@@ -1,4 +1,5 @@
 ﻿/// <reference path="../../typings/phaser/phaser.d.ts" />
+/// <reference path="../../typings/codemirror/codemirror.d.ts" />
 
 //import Phaser = require("phaser");
 import introState = require("FallingShipIntroState");
@@ -11,16 +12,26 @@ module FallingShip {
 
         constructor()
         {
-            this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content');
+            //this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content');
+            this.game = new Phaser.Game("75%", 600, Phaser.AUTO, 'content');
             this.game.state.add("IntroState", introState.GameIntroState, false);
             this.game.state.add("RunningState", runningState.GameRunningState, false);
             this.game.state.start("IntroState", true, true);
+
+            CodeMirror(document.getElementById('codeArea'),
+            {
+                value: "console.log('hello world');",
+                mode: "javascript",
+                theme: "base16-dark",
+                lineNumbers: true
+            });
+
         }
     }
 
     var runningGame = new FallingShip();
 
-
+    
 //});
 }
 
